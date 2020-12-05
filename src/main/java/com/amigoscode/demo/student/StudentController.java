7adc03c39e007d430fdd258f2aa4ee5e64283a24
marianos <marianos8125@gmail.com>
2020-12-05 10:14:25 -0300

    Sending empty Object to Server

--- src/main/java/com/amigoscode/demo/student/StudentController.java
+++ src/main/java/com/amigoscode/demo/student/StudentController.java
@@ -4,6 +4,7 @@ import com.amigoscode.demo.exception.ApiRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,12 +20,12 @@ public class StudentController {
 
     @GetMapping
     public List<Student> getAllStudents() {
-        throw new ApiRequestException("Cannot get all students with custom exception");
-//        return studentService.getAllStudents();
+//        throw new ApiRequestException("Cannot get all students with custom exception");
+        return studentService.getAllStudents();
     }
 
     @PostMapping
-    public void addNewStudent(@RequestBody Student student) {
+    public void addNewStudent(@RequestBody @Valid Student student) {
         studentService.addNewStudent(student);
     }
 }
