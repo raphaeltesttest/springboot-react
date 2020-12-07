2dc7280de4f76a3a6788c63322d4f788a904fd0f
marianos <marianos8125@gmail.com>
2020-12-06 23:19:08 -0300

    JUnit 4

---
+++ src/test/java/com/amigoscode/demo/EmailValidatorTest.java
@@ -0,0 +1,20 @@
+package com.amigoscode.demo;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EmailValidatorTest {
+
+    private final EmailValidator underTest = new EmailValidator();
+
+    @Test
+    public void itShouldValidateCorrectEmail() {
+        assertThat(underTest.test("hello@gmail.com")).isTrue();
+    }
+
+    @Test
+    public void itShouldValidateAnInCorrectEmail() {
+        assertThat(underTest.test("hellogmail.com")).isFalse();
+    }
+}
